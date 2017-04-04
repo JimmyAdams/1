e0182586ca56d6abd4d2822aec3b2c9779b1002e
JimmyAdams <jakub.adamec.svk@gmail.com>
2017-04-04 19:30:32 +0200

    pridana nova funkcia

--- proj1.c
+++ proj1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+int vypis (int n);
+
 int maint(int argc, char *argv[])
 {
+
 	printf("...");
 	return 0;
+}
+
+
+void vypis(int n)
+{
+	for (int i=0; i < n; i++){
+		printf("#");
+	}
 }
