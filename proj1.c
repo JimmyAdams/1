8850f85cb49ea695a1b1b6f6a62b6407be2ee15e
JimmyAdams <jakub.adamec.svk@gmail.com>
2017-04-08 11:35:16 +0200

    upravenie vstupnych argumentov

--- proj1.c
+++ proj1.c
@@ -4,7 +4,13 @@ int vypis (int n);
 
 int maint(int argc, char *argv[])
 {
+	int n;
+	if ( argc = 2)
+	{
+		n = atoi(arv[1])
+	}
 
+	vypis(n);
 	printf("...");
 	return 0;
 }
@@ -15,4 +21,5 @@ void vypis(int n)
 	for (int i=0; i < n; i++){
 		printf("#");
 	}
+	printf("\n");
 }
